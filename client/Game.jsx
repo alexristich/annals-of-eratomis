fe92cef6108354d8c399d42a11600dc1f4dfc74c
Alex Ristich <alexristich@gmail.com>
2016-01-19 21:26:29 -0800

    Tidying up placeholder code which is no longer used

--- client/Game.jsx
+++ client/Game.jsx
@@ -1,16 +1,6 @@
 // Represents the entire application portion of the project
 Game = React.createClass({
 
-    // Temporary fixed set of villains
-    //getVillains() {
-    //    return [
-    //        { _id: 1, type: "melee1"},
-    //        { _id: 2, type: "melee2"},
-    //        { _id: 3, type: "ranged1"},
-    //        { _id: 4, type: "ranged2"}
-    //    ];
-    //},
-
     mixins: [ReactMeteorData],
 
     getMeteorData() {
@@ -26,19 +16,6 @@ Game = React.createClass({
             return <Villain key={villain._id} villain={villain} />;
         })
     },
-
-    // is this function needed if I switch to the LevelPicker component?
-    //handleSubmit(event) {
-    //    event.preventDefault();
-    //    console.log(this.refs);
-    //
-    //    // TODO figure out how to access the unique type value from the DOM node
-    //    var type = ReactDOM.findDOMNode(this.refs.type).value.trim();
-    //    //console.log(ReactDOM.findDOMNode(this.refs.type));
-    //    Villains.insert({
-    //        type: type
-    //    })
-    //},
 
     render() {
         return (
