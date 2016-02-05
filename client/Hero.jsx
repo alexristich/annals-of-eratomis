a23e30562496bac937097ce5f7554dd59f851cc0
Alex Ristich <alexristich@gmail.com>
2016-02-04 23:58:19 -0800

    Adding client-side Hero class to handle rendering

---
+++ client/Hero.jsx
@@ -0,0 +1,24 @@
+// Represents the Hero
+Hero = React.createClass({
+    propTypes: {
+        hero: React.PropTypes.object.isRequired
+    },
+
+    render() {
+        var xpos = this.props.hero.xpos;
+        console.log(this.props.hero.xpos);
+
+        //render hero according to current xpos
+        var imgStyle = {
+            position: 'relative',
+            left: xpos + 'px'
+        };
+
+        return (
+            <div>
+                <img src='/hero.jpg' style=
+                        {imgStyle}/>
+            </div>
+        )
+    }
+});
